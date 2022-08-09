fcbb247f012771c4edd17e667a9cd2580a24ec2f
Dexter Zacour <cdzacour@hotmail.com>
2022-08-09 19:50:26 -0400

    everything finished

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,11 +14,17 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      model: 'product'
+      references: {
+        model: 'product',
+        key: 'id'
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      model: 'tag'
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
